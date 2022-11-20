a465711f5585eeaa044a28330d2fadc90de1ddf0
Ibrahim El Ouadifi <60790882+IbrahimElOuadifi@users.noreply.github.com>
2022-11-20 11:59:48 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const { app, BrowserWindow, ipcMain, dialog } = require('electron')
+const Store = require('electron-store')
 const path = require('path')
 const { readdir } = require('fs/promises')
+const { randexp } = require('randexp')
 
-const devTools = true
+const devTools = false
 
 if (require('electron-squirrel-startup')) {
   app.quit()
@@ -10,8 +12,8 @@ if (require('electron-squirrel-startup')) {
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -40,11 +42,45 @@ app.on('activate', () => {
   }
 })
 
-ipcMain.on('folder-path-select', async ({ reply }) => {
-  const path = dialog.showOpenDialogSync({
-    properties: ['openDirectory'],
-  })
-  const files = await readdir(path[0])
-  reply('folder-path-reply', { files, path: path[0] })
-  console.log(files, path[0])
+ipcMain.handle('folder-path-select', async (_, { defaultPath }) => {
+  const { filePaths: [path], canceled } = await dialog.showOpenDialog({ properties: ['openDirectory'], defaultPath })
+  if(canceled) return console.log('canceled')
+  new Store().set('offers-path', path)
+  // new Store().set('offers-path', path)
+  const files = (await readdir(path, { withFileTypes: true }))
+    .filter((dir) => dir.isDirectory())
+    .map(({ name }) => name)
+  console.log(files, path)
+  return { files, path: path }
 })
+
+ipcMain.handle('get-default-path', async () => {
+  const path = new Store().has('offers-path') ? new Store().get('offers-path') : app.getPath('documents')
+  const files = (await readdir
+  (path, { withFileTypes: true }))
+    .filter((dir) => dir.isDirectory())
+    .map(({ name }) => name)
+  return { files, path }
+})
+
+ipcMain.on('submit-create', ({ reply }, { fields }) => {
+  console.log(fields)
+
+  // selected_folder
+  // region
+  // bucket_name_chars
+  // bucket_name_length
+  // bucket_number
+
+  const buckets = []
+  
+  for(let i = 0; i < fields.bucket_number; i++) {
+    const bucketName = randexp(`[a-z][${fields.bucket_name_chars}][a-z]{${fields.bucket_name_length - 2}}`)
+    buckets.push(bucketName)
+  }
+
+  reply('submitted', { buckets })
+})
+
+
+ipcMain.on('main-close', () => app.quit())
--- src/renderer.js
+++ src/renderer.js
@@ -1,20 +1,48 @@
 const { ipcRenderer } = require('electron')
+const { readdir } = require('fs/promises')
+
+let selected_folder = null
+
+const setSelectedFolder = async (path, filename) => {
+    const files = (await readdir(`${path}\\${filename}`, { withFileTypes: true }))
+    .filter((dir) => dir.isFile())
+    .map(({ name }) => name)
+    Array.from(document.getElementById('folder-details').children).forEach((child) => child.children[0].value === filename ? child.children[0].classList.add('is-dark') : child.children[0].classList.remove('is-dark'))
+    document.querySelector('textarea').value = files.length ? files.join('\r\n') : 'No files found'
+    selected_folder = `${path}\\${filename}`
+}
+
+const setFolderDetail = async (path, files) => {
+    document.getElementById('folder-path').value = path
+    document.getElementById('folder-details').innerHTML = files.map((file) => folderDetailHtml(file)).join('\n')
+    Array.from(document.getElementById('folder-details').children).forEach(child => child.children[0].addEventListener('click', () => setSelectedFolder(path, child.children[0].value)))
+    document.getElementById('folder-details').childElementCount && setSelectedFolder(path, document.getElementById('folder-details').children[0].children[0].value)
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    const folder_path = document.getElementById('folder-path')
-    const folder_detail = document.getElementById('folder-detail')
-
-    folder_path.addEventListener('click', () => ipcRenderer.send('folder-path-select'))
+    document.getElementById('folder-path').addEventListener('click', () => ipcRenderer.invoke('folder-path-select', { defaultPath: document.getElementById('folder-path').value }).then(({ files, path }) => setFolderDetail(path, files)))
+    document.getElementById('main-close').addEventListener('click', () => ipcRenderer.send('main-close'))
+    document.getElementById('main-back').addEventListener('click', () => ipcRenderer.send('main-back'))
+    document.getElementById('main-submit').addEventListener('click', () => {
+        const fields = {
+            selected_folder,
+            region: document.getElementById('select-region').value,
+            bucket_name_chars: Array.from(document.querySelectorAll('.bucket-name-char.is-dark')).map((el) => el.getAttribute('data-pattern')).join(''),
+            bucket_name_length: document.getElementById('bucket-name-length').value,
+            bucket_number: document.getElementById('bucket-number').value
+        }
+        ipcRenderer.send('submit-create', { fields })
+    })
+    Array.from(document.querySelectorAll('.bucket-name-char')).forEach((element) => element.addEventListener('click', () => {
+        if(element.value !== 'LOWERCASE') element.classList.toggle('is-dark')
+    }))
 })
 
-ipcRenderer.on('folder-path-reply', (_, arg) => {
-    console.log(arg)
+const folderDetailHtml = (filename) => `<div class="field"><input class="button is-fullwidth is-small" type="button" value="${filename}"></div>`
+
+ipcRenderer.invoke('get-default-path').then(({ files, path }) => setFolderDetail(path, files))
+
+ipcRenderer.on('submitted', (_, { buckets }) => {
+    document.querySelector('textarea').value = buckets.join('\r\n')
+    selected_folder = null
 })
-
-
-`<div class="field">
-<input
-  class="button is-fullwidth is-small is-dark"
-  type="button"
-  value="Offer Name"
-/>`
